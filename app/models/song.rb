c41f0ec9a352ae7b4def10ec4f534c2854be1d32
github username <github email address>
2019-04-16 12:48:38 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,22 @@ class Song < ActiveRecord::Base
   # add associations here
   belongs_to :artist
   belongs_to :genre
+
+  def artist_name=(artist)
+    # if self.artist.nil?
+    #   nil
+    # else
+       self.artist = Artist.find_or_create_by(name: artist)
+
+    # end
+  end
+
+  def artist_name
+     if self.artist.nil?
+       nil
+     else
+       self.artist.name
+     end
+  end
+
 end
